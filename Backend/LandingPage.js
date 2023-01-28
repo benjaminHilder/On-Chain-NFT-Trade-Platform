dfe74774d9e9568fca71919d3c2e8788d85ce271
Benjamin Hilder <benhilder7@gmail.com>
2023-01-28 13:22:46 +1030

    Frontend correct interacts with smart contract
    Can now place a trade order in successfully

--- Backend/LandingPage.js
+++ Backend/LandingPage.js
@@ -3,8 +3,244 @@ export let signer
 export let signerAddress
 export let recipientAddress
 
-const tradeContractAddress = 0;
-const tradeABI = [];
+const tradeContractAddress = "0x2Fd136348FeF6BFD12CF5803e914dfeF665A9fA8";
+const tradeABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tradeOfferIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "acceptTrade",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_recipientAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address[]",
+				"name": "_requesterNftAddresses",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_requesterNftIDs",
+				"type": "uint256[]"
+			},
+			{
+				"internalType": "address[]",
+				"name": "_recipientNftAddresses",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_recipientNftIDs",
+				"type": "uint256[]"
+			}
+		],
+		"name": "createTradeRequest",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_tradeOfferIndex",
+				"type": "uint256"
+			}
+		],
+		"name": "declineTrade",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_index",
+				"type": "uint256"
+			}
+		],
+		"name": "excuteTrade",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_index",
+				"type": "uint256"
+			}
+		],
+		"name": "giveContractAccessToNFTs",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "getAllOffers",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "address",
+						"name": "requesterAddress",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "recipientAddress",
+						"type": "address"
+					},
+					{
+						"internalType": "address[]",
+						"name": "requesterNftAddresses",
+						"type": "address[]"
+					},
+					{
+						"internalType": "uint256[]",
+						"name": "requesterNftIDs",
+						"type": "uint256[]"
+					},
+					{
+						"internalType": "address[]",
+						"name": "recipientNftAddresses",
+						"type": "address[]"
+					},
+					{
+						"internalType": "uint256[]",
+						"name": "recipientNftIDs",
+						"type": "uint256[]"
+					},
+					{
+						"internalType": "uint256",
+						"name": "requesterIndex",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "recipientIndex",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "timestamp",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bool",
+						"name": "active",
+						"type": "bool"
+					},
+					{
+						"internalType": "bool",
+						"name": "result",
+						"type": "bool"
+					},
+					{
+						"internalType": "bool",
+						"name": "requesterReady",
+						"type": "bool"
+					},
+					{
+						"internalType": "bool",
+						"name": "recipientReady",
+						"type": "bool"
+					}
+				],
+				"internalType": "struct NFTTrade.TradeInfo[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tradeOffers",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "requesterAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "recipientAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "requesterIndex",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "recipientIndex",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "active",
+				"type": "bool"
+			},
+			{
+				"internalType": "bool",
+				"name": "result",
+				"type": "bool"
+			},
+			{
+				"internalType": "bool",
+				"name": "requesterReady",
+				"type": "bool"
+			},
+			{
+				"internalType": "bool",
+				"name": "recipientReady",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
 
 let nftBoxes = document.querySelector(".UserNFTs");
 
@@ -12,6 +248,12 @@ let RecipientTradeBox = document.getElementById("RecipientTradeBox")
 
 let UserSelected = new Set()
 let RecipientSelected = new Set()
+
+//let userSelectedAddresses = []
+//let userSelectedIDs = []
+//
+//let recipientSelectedAddresses = []
+//let recipientSelectedIDs = []
 
 class nftInfo {
   constructor(contractAddress, ID, ImgURL) {
@@ -33,18 +275,49 @@ async function createTradeRequest() {
   let recipientNftAddresses = []
   let recipientNftIDs = []
 
-  for (let i = 0; i < UserSelected.length; i++) {
-    requesterNftAddresses.push(UserSelected[i].contractAddress)
-    requesterNftIDs.push(UserSelected[i].ID)
+  let UserSelectedArray = [...UserSelected];
+  let RecipientSelectedArray = [...RecipientSelected]
+
+  //for (let value of UserSelected) {
+  //  console.log(`val ${JSON.stringify(value).contractAddress}`);
+  //}
+//
+  ////console.log("asda " + UserSelected[0])
+  for (let i = 0; i < UserSelectedArray.length; i++) {
+    //console.log(`contract addresses ${UserSelectedArray[i].contractAddress.address}`)
+    //console.log(`id ${UserSelectedArray[i].ID}`)
+    requesterNftAddresses.push(UserSelectedArray[i].contractAddress.address)
+    requesterNftIDs.push(UserSelectedArray[i].ID)
+  }
+//
+  for (let i = 0; i < RecipientSelectedArray.length; i++) {
+    //console.log(`contract addresses ${RecipientSelectedArray[i].contractAddress.address}`)
+    //console.log(`id ${RecipientSelectedArray[i].ID}`)
+
+    recipientNftAddresses.push(RecipientSelectedArray[i].contractAddress.address)
+    recipientNftIDs.push(RecipientSelectedArray[i].ID)
   }
 
-  for (let i = 0; i < RecipientSelected.length; i++) {
-    recipientNftAddresses.push(RecipientSelected[i].contractAddress)
-    recipientNftIDs.push(RecipientSelected[i].ID)
-  }
+  console.log(requesterNftAddresses)
 
+  //let solRequesterNftAddresses = ethers.utils.arrayify(requesterNftAddresses);
+  //let solRequesterNftIDs = ethers.utils.arrayify(requesterNftIDs);
+  //let solRecipientNftAddresses = ethers.utils.arrayify(recipientNftAddresses);
+  //let solRecipientNftIDs = ethers.utils.arrayify(recipientNftIDs);
+//
+  console.log(recipientAddress)
+//
+  console.log(`req add ${requesterNftAddresses}`)
+  console.log(`req id ${requesterNftIDs}`)
+  console.log(`rec add ${recipientNftAddresses}`)
+  console.log(`rec id ${recipientNftIDs}`)
+//
+  //let address = await ethers.utils.getAddress(recipientAddress)
+
+
+//
   const contract = await new ethers.Contract(tradeContractAddress, tradeABI, provider);
-  const transaction = await contract.connect(signer).createTradeRequest(recipientAddress, requesterNftAddresses, requesterNftIDs, recipientNftAddresses, recipientNftIDs)
+  const transaction = await contract.connect(signer).createTradeRequest(ethers.utils.getAddress(recipientAddress), recipientNftAddresses, recipientNftIDs, requesterNftAddresses, requesterNftIDs)
 }
 
 export async function connectMetamask() {
@@ -74,11 +347,48 @@ export async function connectMetamask() {
 }
 
 async function displayRecipientNfts() {
-    let inputField = await document.getElementById("chosenRecipientAddress")
-    recipientAddress = inputField;
+  let inputField = await document.getElementById("chosenRecipientAddress")
+  let inputValue = await inputField.value;
+  recipientAddress = await inputValue.toString();
+
+  axios.get(`https://testnets-api.opensea.io/api/v1/assets?owner=${recipientAddress}&limit=50`)
+  .then(function (response) {
+    
+    //console.log(`respose ${JSON.stringify(response)}`)
+   // console.log(`addresses: ${JSON.stringify(response)}`)
+    let nfts = response.data.assets;
+    
+    for (let i = 0; i < nfts.length; i++) {
+        let nft = nfts[i];
+        let nftId = nft.token_id;
+        let nftTokenContractAddress = nft.asset_contract;
+        let nftImageUrl = nft.image_url; // add this line to get the image url
+
+        let newNft = new nftInfo(nftTokenContractAddress, nftId, nftImageUrl);
+        //console.log(`new nft: ${JSON.stringify(newNft)}`)
+        // create a new div for this NFT
+        let nftBox = document.createElement("div");
+        nftBox.className = "nftBox"
+        nftBox.classList.add("nftBox");
+        nftBox.addEventListener("click", function() {
+            //this.classList.toggle("selected");
+            
+            addToSelected(RecipientSelected, newNft, nftBox)
+        });
+
+        nftBox.style.backgroundImage = 'url(' + nftImageUrl + ')';
+    
+        RecipientTradeBox.appendChild(nftBox);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
 
     let userAddress = await inputField.value;
-    console.log(`user address ${userAddress}`)
+    //console.log(`user address ${userAddress}`)
 
     let backButton = await document.createElement("button");
     backButton.textContent = "back";
@@ -88,6 +398,10 @@ async function displayRecipientNfts() {
     backButton.style.position = "fixed";
     backButton.style.left = "140vh";
 
+    await backButton.addEventListener("click", function() {
+      setupFirstRecipientTradeBox(backButton, createTradeOfferButton);
+    });
+
     let createTradeOfferButton = await document.createElement("button")
     createTradeOfferButton.textContent = "Create Trade Offer"
     createTradeOfferButton.className = "tradeOfferButton"
@@ -95,9 +409,9 @@ async function displayRecipientNfts() {
     createTradeOfferButton.style.position = "fixed";
     createTradeOfferButton.style.left = "45.5%";
 
-    await backButton.addEventListener("click", function() {
-      setupFirstRecipientTradeBox(backButton, createTradeOfferButton);
-    });
+    await createTradeOfferButton.addEventListener("click", function() {
+      createTradeRequest();
+    })
     
     RecipientBoxTitle.insertBefore(backButton, RecipientBoxTitle.firstChild)
     RecipientBoxTitle.insertBefore(createTradeOfferButton, RecipientBoxTitle.firstChild)
@@ -105,41 +419,10 @@ async function displayRecipientNfts() {
     let chooseRecipientAddress = await document.getElementById("chooseRecipientTradeBox")
     await chooseRecipientAddress.remove();
 
-    axios.get(`https://testnets-api.opensea.io/api/v1/assets?owner=${userAddress}&limit=50`)
-      .then(function (response) {
-
-        let nfts = response.data.assets;
-        
-        for (let i = 0; i < nfts.length; i++) {
-            let nft = nfts[i];
-            let nftId = nft.token_id;
-            let nftTokenContractAddress = nft.contract_address;
-            let nftImageUrl = nft.image_url; // add this line to get the image url
-
-            let newNft = new nftInfo(nft.contract_address, nft.token_id, nft.image_url);
-
-            // create a new div for this NFT
-            let nftBox = document.createElement("div");
-            nftBox.className = "nftBox"
-            nftBox.classList.add("nftBox");
-            nftBox.addEventListener("click", function() {
-                //this.classList.toggle("selected");
-                addToSelected(RecipientSelected, newNft)
-            });
-
-            nftBox.style.backgroundImage = 'url(' + nftImageUrl + ')';
-        
-            RecipientTradeBox.appendChild(nftBox);
-
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+ 
 }
 
 async function setupFirstRecipientTradeBox(backBtn, tradeOfferBtn) {
-  console.log(`child count ${RecipientTradeBox.children.length}`)
   //while there is still a child
   while (RecipientTradeBox.firstChild) {
     RecipientTradeBox.removeChild(RecipientTradeBox.firstChild)
@@ -149,17 +432,28 @@ async function setupFirstRecipientTradeBox(backBtn, tradeOfferBtn) {
   
   backBtn.parentNode.removeChild(backBtn);
   tradeOfferBtn.parentNode.removeChild(tradeOfferBtn);
+
+  UserSelected.clear();
+  RecipientSelected.clear();
 }
 
-async function addToSelected(set, nft) {
+async function addToSelected(set, nft, nftBox) {
   if (nft.Selected == false) {
-    set.add(nft);
+
+    set.add(nft)
     nft.Selected = true;
+
+    nftBox.style.filter = "brightness(70%)"
   
   } else {
+
     set.delete(nft)
     nft.Selected = false;
+
+    nftBox.style.filter = "brightness(100%)"
   }
+
+  console.log([...set])
 }
 
 async function chooseRecipientTradeBox() {
@@ -203,25 +497,27 @@ async function createNFTBoxes() {
     //call again
     axios.get(`https://testnets-api.opensea.io/api/v1/assets?owner=${userAddress}&limit=50`)
       .then(function (response) {
-
+ 
         let nfts = response.data.assets;
-        
+
         for (let i = 0; i < nfts.length; i++) {
             let nft = nfts[i];
+   
             let nftId = nft.token_id;
-            let nftTokenContractAddress = nft.contract_address;
+            let nftTokenContractAddress = nft.asset_contract;
             let nftImageUrl = nft.image_url; // add this line to get the image url
 
-            let newNft = new nftInfo(nft.contract_address, nft.token_id, nft.image_url);
-
+            let newNft = new nftInfo(nftTokenContractAddress, nftId, nftImageUrl);
+            //console.log(`new nft: ${JSON.stringify(newNft)}`)
             // create a new div for this NFT
             let nftBox = document.createElement("div");
             nftBox.className = "nftBox"
             nftBox.classList.add("nftBox");
             nftBox.addEventListener("click", function() {
                 //this.classList.toggle("selected");
-                addToSelected(UserSelected, newNft)
-            });
+                
+                addToSelected(UserSelected, newNft, nftBox)
+              });
 
             nftBox.style.backgroundImage = 'url(' + nftImageUrl + ')';
         
